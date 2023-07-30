19ffc9df45a7cf3eec1ad2f64ac07f8f0e5a4f65
Bouclet Emeline <mimi@MacBook-Air-de-Bouclet.local>
2023-07-30 19:13:18 +0200

    mise en indisponbilité de l'offre => nouveau modèle avec les offres vendues

--- routes/payment.js
+++ routes/payment.js
@@ -5,12 +5,15 @@ const router = express.Router(); // Utilisez express.Router() pour créer un rou
 router.use(express.json());
 router.use(cors());
 
+const User = require("../models/User");
+const Offer = require("../models/Offer");
+
 const isAuthenticated = require("../middleware/isAuthenticated");
 
 router.post("/payment", isAuthenticated, async (req, res) => {
 	console.log("pay, req.body", req.body);
 	try {
-		const stripeToken = req.body.stripeToken;
+		const stripeToken = req.body.token;
 
 		const response = await stripe.charges.create({
 			amount: req.body.amount,
@@ -20,7 +23,20 @@ router.post("/payment", isAuthenticated, async (req, res) => {
 		});
 		console.log(response.status);
 	} catch (error) {
-		console.log("erreur", error);
+		console.log("erreur payment", error);
+	}
+	// je recherche l'offre dont j'ai envoyé l'ID depuis le front dans req.body.offer_id
+	try {
+        const offerToUpdate = await Offer.findOne({
+            _id : req.body.offer_id
+        });
+        // RESTE A FAIRE
+        // Etape 1 : récupérer l'offre
+        // Etape 2 : la créer dans un autre modèle qu'on pourra appeler SoldOffer avec les donénes de l'acheteur
+        // Etape 2 bis : définir le modèle de SoldOffer
+        // Etape 3 : supprimer l'offre du modèle Offer
+	} catch (error) {
+		console.log("erreur actualisation offre en BDD", error);
 	}
 });
 
